c4beeb939ce9f5634067b7ce703cda00fc40b065
Febin Baiju <febin129@gmail.com>
2020-02-16 16:56:00 +0530

    Added MainWindow and WebCam Record Functions

---
+++ MainWindowRun.py
@@ -0,0 +1,28 @@
+from PyQt5 import QtWidgets, uic
+from WebCamRecord import Ui2
+import sys
+#import Settings
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('mainwindow.ui', self)
+
+        self.button = self.findChild(QtWidgets.QPushButton, 'pbStartVideoCapture')
+        self.button.clicked.connect(self.openCaptureWindow)
+
+        self.exitButton = self.findChild(QtWidgets.QPushButton,'pbExit')
+        self.exitButton.clicked.connect(self.close)
+        self.show()
+
+    def printButtonPressed(self):
+        # This is executed when the button is pressed
+        print('printButtonPressed')
+    
+    def openCaptureWindow(self):
+        self.w = Ui2()
+        self.w.show()
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
---
+++ WebCamRecord.py
@@ -0,0 +1,57 @@
+from PyQt5 import QtWidgets, uic
+import sys
+import cv2
+from PyQt5.QtWidgets import  QApplication,QWidget, QLabel
+from PyQt5.QtCore import QThread, Qt, pyqtSignal, pyqtSlot
+from PyQt5.QtGui import QIcon,QImage, QPixmap
+#import Settings
+
+class Thread(QThread):
+    changePixmap = pyqtSignal(QImage)
+
+    def run(self):
+        cap = cv2.VideoCapture(0)
+        # Get the Default resolutions
+        frame_width = int(cap.get(3))
+        frame_height = int(cap.get(4))
+        
+        # Define the codec and filename.
+        out = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 25, (frame_width,frame_height))
+
+        while True:
+            ret, frame = cap.read()
+            if ret:
+                rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                out.write(frame)
+                h, w, ch = rgbImage.shape
+                bytesPerLine = ch * w
+                convertToQtFormat = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
+                p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
+                self.changePixmap.emit(p)
+
+
+class Ui2(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui2, self).__init__()
+        uic.loadUi('capturewindow.ui', self)
+        self.resize(900,600)
+        self.label = self.findChild(QtWidgets.QLabel, 'label')
+        self.label.resize(640, 480)
+        self.stopbutton = self.findChild(QtWidgets.QPushButton,'pbStopCapture')
+        self.stopbutton.move(420,440)
+        self.stopbutton.clicked.connect(self.close)
+        th = Thread(self)
+        th.changePixmap.connect(self.setImage)
+        th.start()
+        self.show()
+
+    @pyqtSlot(QImage)
+    def setImage(self, image):
+        self.label.setPixmap(QPixmap.fromImage(image))
+
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = Ui2()
+    app.exec_()
